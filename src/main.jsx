e1b49d0f090c11d25e0bf502c868bf9f4281a2f1
JairoAlexandro <jairosf3113@gmail.com>
2025-03-05 19:29:40 +0100

    Fix formatting: add a blank line for improved readability in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Router } from './router/Router.jsx';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className="flex flex-col min-h-screen">
